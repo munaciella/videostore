27bb88594ed98b413715b6e6f293daf7f79fa259
munaciella <franes88@gmail.com>
2025-02-21 15:26:20 +0000

    add header to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
-          //disableTransitionOnChange
+          disableTransitionOnChange
         >
           <Header />
 
